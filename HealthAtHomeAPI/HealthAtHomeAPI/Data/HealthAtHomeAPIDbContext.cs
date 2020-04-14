40542840100bbf87f7e63858c03918c89989e9c6
Sue Tarazi <suemachtley@gmail.com>
2020-04-14 11:33:12 -0700

    updated seeded exercises

--- HealthAtHomeAPI/HealthAtHomeAPI/Data/HealthAtHomeAPIDbContext.cs
+++ HealthAtHomeAPI/HealthAtHomeAPI/Data/HealthAtHomeAPIDbContext.cs
@@ -103,9 +103,181 @@ namespace HealthAtHomeAPI.Data
                     Reps = 10,
                     Image = "test",
                     Description = "Bring weight shoulders, straighten arms pressing weight over head."
+                },
+
+
+
+                    new Exercises
+                    {
+                        ID = 6,
+                        ExerciseName = "Squat",
+                        Sets = 3,
+                        Reps = 10,
+                        Image = "test",
+                        Description = "Stand with feet slightly wider than shoulder width apart, sit back in an imaginary chair. Press feet into the ground to stand back up."
+                    },
+                new Exercises
+                {
+                    ID = 7,
+                    ExerciseName = "Lunges",
+                    Sets = 3,
+                    Reps = 10,
+                    Image = "test",
+                    Description = "Stand in good posture. Step forward and bend both knees to 90 degrees, keeping trunk upright."
+                },
+                new Exercises
+                {
+                    ID = 8,
+                    ExerciseName = "Sidelying Leg lifts",
+                    Sets = 3,
+                    Reps = 10,
+                    Image = "test",
+                    Description = "Begin lying on your side with bottom knee bent and top leg straight in line with your trunk. Hold a weight on the outside of your thigh. Lift your top leg up and back, keeping your knee straight."
+                },
+                new Exercises
+                {
+                    ID = 9,
+                    ExerciseName = "Calf Raises",
+                    Sets = 3,
+                    Reps = 10,
+                    Image = "test",
+                    Description = "Place the balls of your feet at the edge of a stair step. Lower your heels below the plane of the step, then push into the balls of your feet to rise back up."
+                },
+                new Exercises
+                {
+                    ID = 10,
+                    ExerciseName = "Good Mornings",
+                    Sets = 3,
+                    Reps = 10,
+                    Image = "test",
+                    Description = "Stand in good posture, weight optional. Bend forward, keeping knees straight and back flat. Return to standing, keeping your knees straight."
+                },
+
+                    new Exercises
+                    {
+                        ID = 11,
+                        ExerciseName = "Bridging with leg extensions",
+                        Sets = 3,
+                        Reps = 10,
+                        Image = "test",
+                        Description = "Lie on your back with knees bent and feet flat. Lift glutes off floor and keeping hips square, straighten one leg. Lower back down and repeat on the other side."
+                    },
+                new Exercises
+                {
+                    ID = 12,
+                    ExerciseName = "Arm and Leg Extension on All Fours",
+                    Sets = 3,
+                    Reps = 10,
+                    Image = "test",
+                    Description = "Begin on hands and knees, hands underneath shoulders, knees underneath hips. Lift opposite arm and leg. Hold for 3 seconds, then lower back down."
+                },
+                new Exercises
+                {
+                    ID = 13,
+                    ExerciseName = "Plank",
+                    Sets = 3,
+                    Reps = 10,
+                    Image = "test",
+                    Description = "Begin on hands and knees, straighten one leg onto the ball of your foot, then the other. Squeeze gluteals and press hands into the floor, keeping your trunk and pelvis in neutral. Hold for up to 1 minute."
+                },
+                new Exercises
+                {
+                    ID = 14,
+                    ExerciseName = "Pilates March",
+                    Sets = 3,
+                    Reps = 10,
+                    Image = "test",
+                    Description = "Lie on your back and alternate lifting your legs to hip height. Keep your knees bent."
+                },
+                new Exercises
+                {
+                    ID = 15,
+                    ExerciseName = "Trunk Twists",
+                    Sets = 3,
+                    Reps = 10,
+                    Image = "test",
+                    Description = "Sit with trunk straight and feet flat on the floor. Lean back slightly, holding a weight (optional). Move the weight from the left side of your body to the right side of your body back and forth."
+                },
+
+                    new Exercises
+                    {
+                        ID = 16,
+                        ExerciseName = "Cat & Cow",
+                        Sets = 1,
+                        Reps = 10,
+                        Image = "test",
+                        Description = "Begin on hands and knees. Lower your belly and lift your head, then reverse the motion by rounding your back towards the ceiling and dropping your head. Repeat back and forth."
+                    },
+                new Exercises
+                {
+                    ID = 17,
+                    ExerciseName = "Downward Dog",
+                    Sets = 1,
+                    Reps = 3,
+                    Image = "test",
+                    Description = "Begin on hands and knees. Push down into your hands and knees, lifting your trunk into the air. Breathe deeply."
+                },
+                new Exercises
+                {
+                    ID = 18,
+                    ExerciseName = "Chattaranga",
+                    Sets = 3,
+                    Reps = 1,
+                    Image = "test",
+                    Description = "Begin in a plank position with hands slightly wider than shoulder width apart, keeping your core straight, bend your elbows to your sides, lowering yourself to the floor."
+                },
+                new Exercises
+                {
+                    ID = 19,
+                    ExerciseName = "Cobra",
+                    Sets = 3,
+                    Reps = 1,
+                    Image = "test",
+                    Description = "Begin lying on your stomach with hands at your shoulders. Press into your hands and lift your trunk off the floor."
+                },
+                new Exercises
+                {
+                    ID = 20,
+                    ExerciseName = "Warrior I",
+                    Sets = 3,
+                    Reps = 1,
+                    Image = "test",
+                    Description = "From a downward dog position, bring one foot between your hands and stand into a lunge."
+                },
+
+                new Exercises
+                {
+                    ID = 21,
+                    ExerciseName = "Boat Pose",
+                    Sets = 3,
+                    Reps = 1,
+                    Image = "test",
+                    Description = "Sit with trunk straight and feet flat on the floor. Lean back slightly, and lift legs off the floor."
+                },
+
+                new Exercises
+                {
+                    ID = 22,
+                    ExerciseName = "Quad Stretch",
+                    Sets = 3,
+                    Reps = 1,
+                    Image = "test",
+                    Description = "Stand in good posture. Using your hand, bring your foot towards your gluteals, keeping knees together."
+                },
+
+                new Exercises
+                {
+                    ID = 23,
+                    ExerciseName = "Adductor Stretch",
+                    Sets = 3,
+                    Reps = 1,
+                    Image = "test",
+                    Description = "Perform a side lunge and allow the muscles on your inner thigh to stretch."
                 }
                 );
 
+
+            // Allyson and Sue to seed Routine in a bit 
             modelBuilder.Entity<Routine>().HasData(
                  new Routine
                  {
@@ -114,6 +286,21 @@ namespace HealthAtHomeAPI.Data
                  }
                  );
 
+            // Brandon - please seed data for Rating
+            modelBuilder.Entity<Rating>().HasData(
+                 new Rating
+                 {
+                     
+                 }
+                 );
+
+            // Brandon - please seed data for User
+            modelBuilder.Entity<User>().HasData(
+                 new User
+                 {
+                     
+                 }
+                 );
 
 
         }
